c34ff057bb45eedcd0337533851d125c78da06d8
robertdinaro <adeelshadman@hotmail.com>
2014-03-29 12:22:27 +0000

    Command.cs

---
+++ ParserExtractor/Ropnoy.Lattice.Domain/Command.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ropnoy.Lattice.Domain
+{
+    public class Command
+    {
+        public int Id { get; set; }
+
+        public Call Call { get; set; }
+
+        public Cell Cell { get; set; }
+
+        public Layout Layout { get; set; }
+
+        public bool IsValid { get; set; }
+
+        public List<Argument> Arguments { get; set; } 
+    }
+}
